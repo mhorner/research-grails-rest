fde9a6c54d586a881cef53a72519f8e948d5d340
Matt Horner <mhorner7@gmail.com>
2012-01-22 13:23:08 -0600

    Added ISBN and author to the Book model.

--- grails-app/domain/com/blogspot/mhorner/demo/Book.groovy
+++ grails-app/domain/com/blogspot/mhorner/demo/Book.groovy
@@ -4,9 +4,13 @@ class Book {
 
 	String title
 	String description
+	String author
+	String iSBN
 	
     static constraints = {
 		title(blank: false)
 		description(blank: false)
+		author(blank: false)
+		iSBN(blank: false)
     }
 }
